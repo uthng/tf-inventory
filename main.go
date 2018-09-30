0abb20deab0fa4bd2de493362c5e08b80cd6c151
uthng <u.nguyenthanh@gmail.com>
2018-10-01 01:32:45 +0200

    - Remove printHelp to use flag.Usage

--- main.go
+++ main.go
@@ -10,15 +10,6 @@ import (
 )
 
 // printHelp print command's help
-func printHelp() {
-	fmt.Println("")
-	fmt.Printf("%s is a utility to generate ansible inventory file from Terraform state\n", os.Args[0])
-	fmt.Printf("Syntax: %s [options] [hostname] [tfstate]\n", os.Args[0])
-	fmt.Printf("\t-help: Print this help\n")
-	fmt.Printf("\t-list: Generate ansible inventory file under JSON format in list mode\n")
-	fmt.Printf("\t-host: Generate ansible inventory file under JSON format in host mode\n")
-	fmt.Printf("\t-inventory: Generate ansible inventory file\n")
-}
 
 func main() {
 	var res interface{}
@@ -26,29 +17,29 @@ func main() {
 
 	tfsFile := ""
 
-	help := flag.Bool("help", false, "Print help")
 	list := flag.Bool("list", false, "List mode")
 	host := flag.String("host", "", "Host mode")
 	//inventory := flag.Bool("inventory", false, "Inventory mode")
+
+	flag.Usage = func() {
+		fmt.Println("")
+		fmt.Printf("%s is a utility to generate ansible inventory file from Terraform state\n", os.Args[0])
+		fmt.Printf("Syntax: %s [options] [hostname] [tfstate]\n", os.Args[0])
+		flag.PrintDefaults()
+	}
 
 	flag.Parse()
 
 	// Check command line flags
 	if flag.NFlag() <= 0 || flag.NFlag() > 1 {
 		fmt.Fprintf(os.Stderr, "Error: you must specify one and only one argument")
-		printHelp()
 		os.Exit(1)
-	}
-
-	if *help {
-		printHelp()
 	}
 
 	if *list {
 
 		if len(os.Args) > 3 {
 			fmt.Fprintf(os.Stderr, "Error: only 1 arguments at most are accepted!\n")
-			printHelp()
 			os.Exit(1)
 		}
 
@@ -75,13 +66,11 @@ func main() {
 	if *host != "" {
 		if len(os.Args) > 4 {
 			fmt.Fprintf(os.Stderr, "Error: only 2 arguments at most are accepted!\n")
-			printHelp()
 			os.Exit(1)
 		}
 
 		if len(os.Args) < 3 {
 			fmt.Fprintf(os.Stderr, "Error: You must specify at least a hostname as argument!\n")
-			printHelp()
 			os.Exit(1)
 		}
 
