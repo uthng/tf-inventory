888c99e49304d9ac73f07d5473e3b553108b1c96
Thanh NGUYEN <u.nguyenthanh@gmail.com>
2019-08-11 02:44:49 +0200

    feat(common): Use kingpin for commandline instead of flag
    Fixed #8

--- main.go
+++ main.go
@@ -2,96 +2,50 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
+	//"flag"
 	"fmt"
 	"os"
 
 	"github.com/uthng/tf-inventory/inventory"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-// printHelp print command's help
+var (
+	app     = kingpin.New("tf-inventory", "An utility to generate ansible inventory file from Terraform state")
+	list    = app.Flag("list", "List mode").Bool()
+	host    = app.Flag("host", "Host mode").String()
+	tfsFile = app.Arg("tfsfile", "Terraform state file").String()
+)
 
 func main() {
-	var res interface{}
-	var err error
 
-	tfsFile := ""
+	_, err := app.Parse(os.Args[1:])
+	app.FatalIfError(err, "")
 
-	list := flag.Bool("list", false, "List mode")
-	host := flag.String("host", "", "Host mode")
-	//inventory := flag.Bool("inventory", false, "Inventory mode")
-
-	flag.Usage = func() {
-		fmt.Println("")
-		fmt.Printf("%s is a utility to generate ansible inventory file from Terraform state\n", os.Args[0])
-		fmt.Printf("Syntax: %s [options] [hostname] [tfstate]\n", os.Args[0])
-		flag.PrintDefaults()
-	}
-
-	flag.Parse()
-
-	// Check command line flags
-	if flag.NFlag() <= 0 || flag.NFlag() > 1 {
-		fmt.Fprintf(os.Stderr, "Error: you must specify one and only one argument")
-		os.Exit(1)
+	if *list && (*host != "") {
+		app.Fatalf("The flag --list cannot be used with --host")
 	}
 
 	if *list {
-
-		if len(os.Args) > 3 {
-			fmt.Fprintf(os.Stderr, "Error: only 1 arguments at most are accepted!\n")
-			os.Exit(1)
-		}
-
-		if len(os.Args) == 3 {
-			tfsFile = os.Args[2]
-		}
-
-		res, err = inventory.ExecCmd("list", "", tfsFile)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, err.Error())
-			os.Exit(1)
-		}
+		res, err := inventory.ExecCmd("list", "", *tfsFile)
+		app.FatalIfError(err, "")
 
 		b, err := json.Marshal(res)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, err.Error())
-			os.Exit(1)
-		}
+		app.FatalIfError(err, "")
 
 		fmt.Fprintf(os.Stdout, string(b))
-		os.Exit(0)
+		app.Terminate(nil)
 	}
 
 	if *host != "" {
-		if len(os.Args) > 4 {
-			fmt.Fprintf(os.Stderr, "Error: only 2 arguments at most are accepted!\n")
-			os.Exit(1)
-		}
-
-		if len(os.Args) < 3 {
-			fmt.Fprintf(os.Stderr, "Error: You must specify at least a hostname as argument!\n")
-			os.Exit(1)
-		}
-
-		if len(os.Args) == 4 {
-			tfsFile = os.Args[3]
-		}
-
-		res, err = inventory.ExecCmd("host", os.Args[2], tfsFile)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, err.Error())
-			os.Exit(1)
-		}
+		res, err := inventory.ExecCmd("host", *host, *tfsFile)
+		app.FatalIfError(err, "")
 
 		b, err := json.Marshal(res)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, err.Error())
-			os.Exit(1)
-		}
+		app.FatalIfError(err, "")
 
 		fmt.Fprintf(os.Stdout, string(b))
-		os.Exit(0)
+		app.Terminate(nil)
 	}
 
 	//if *inventory {
